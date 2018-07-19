881f31bdb7f3817af5992b7d5f79811ed6fded10
terrencewei <terrencewei@aaxischina.com>
2018-07-19 17:24:10 +0800

    Using egg-redis plugin instead of redis module

--- app/service/seckill.js
+++ app/service/seckill.js
@@ -1,20 +1,19 @@
 // app/service/seckill.js
 const Service = require('egg').Service
-// redis
-const Redis = require('ioredis')
 // kafka-node
 const kafka = require('kafka-node')
 const producer = new kafka.Producer(new kafka.Client('localhost:2181'))
 
 class SeckillService extends Service {
   async buy () {
-    const res = await this.processRedis()
+    const res = await this.consumeStockInRedis()
     return res
   }
 
-  async processRedis (redis) {
+  async consumeStockInRedis (redis) {
     if (redis == null || redis === 'undefined') {
-      redis = new Redis()
+      const app = this.app
+      redis = await app.redis.createInstanceAsync(app.config.redis.client)
     }
     redis.watch('counter')
     const stock = await redis.get('counter')
@@ -25,10 +24,10 @@ class SeckillService extends Service {
       if (results != null) {
         // means no conflict, counter has been decreased
         // then produce a message via kafka to real decrease the stock in mysql DB
-        this.sendKafka()
-        return results
+        await this.sendKafka()
+        return results[0][1]
       } else {
-        const res = await this.processRedis(redis)
+        const res = await this.consumeStockInRedis(redis)
         return res
       }
     }
